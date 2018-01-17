bd6572edfdb566aba4d025968a6db86401726944
Paweł Koszelew <koszelew@gmail.com>
2018-01-17 01:21:43 +0100

    Added simple naive Bayes classification

--- run.R
+++ run.R
@@ -1,5 +1,8 @@
 #!/usr/bin/env Rscript
 
+library(caret)
+library(klaR)
+
 #grouping function
 group <- function(dataset, k){
   # K-Means Cluster Analysis
@@ -16,7 +19,7 @@ d1=read.table("student-mat.csv",sep=",",header=TRUE)
 # portugalski
 d2=read.table("student-por.csv",sep=",",header=TRUE)
 # lista unikalnych studentów
-d3=merge(d1,d2, by=c("school","sex","age","address","famsize","Pstatus","Medu","Fedu","Mjob","Fjob","reason","nursery","internet"), all = TRUE)
+d3=merge(d1,d2,by=c("school","sex","age","address","famsize","Pstatus","Medu","Fedu","Mjob","Fjob","reason","nursery","internet"))
 
 
 # selecting fatures
@@ -26,7 +29,21 @@ selected_fatures <- na.omit(selected_fatures) # listwise deletion of missing
 #selected_fatures <- scale(selected_fatures) # standardize variables]
 
 grouped <- data.frame(selected_fatures, group_id=group(selected_fatures, 2))
-
 plot(grouped$Dalc, grouped$Walc, col=grouped$group_id)
 #library(rgl)
 #plot3d(mydata$Dalc, mydata$Walc, mydata$absences, col=mydata$fit.cluster)
+
+# Bayes
+# Utworzenie dataseta z etykietami z grupowania
+students <- d3
+students$Labels <- as.factor(grouped$group_id)
+# Metoda trenowania modelu
+train_control <- trainControl(method="cv", number=10)
+# Tuning modelu
+grid <- data.frame(fL=c(0, 0.5, 1.0), usekernel = c(TRUE, TRUE, TRUE), adjust = c(1.0, 1.0, 1.0))
+# Trening modelu
+model <- train(Labels~., data=students, trControl=train_control, tuneGrid=grid, method="nb")
+# Wyniki
+print(model)
+predict(model, students[3,], type = "prob")
+
