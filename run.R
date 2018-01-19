7101b4d58257383ed7639a3829d29f45f8511d87
Paweł Koszelew <koszelew@gmail.com>
2018-01-19 13:41:27 +0100

    Added grouping CSVs, changed bayes rejection tests, changed gitignore

--- run.R
+++ run.R
@@ -16,18 +16,20 @@ group <- function(dataset, k){
 dist_group <- function(dataset, k, dist_method, clust_method){
   # Ward Hierarchical Clustering
   d <- dist(dataset, method = dist_method) # distance matrix
-  fit <- hclust(d, method=clust_method) 
+  fit <- hclust(d, method=clust_method)
+  png(file = paste("grouping_plots/dendograms/",clust_method,"_",dist_method,".png", sep=""), width = 8, height = 8, units = 'in', res = 400)
   plot(fit) # display dendogram
   groups <- cutree(fit, k=k) # cut tree into 5 clusters
   # draw dendogram with red borders around the 5 clusters 
-  rect.hclust(fit, k=k, border="red")  
+  rect.hclust(fit, k=k, border="red")
+  dev.off()
   return(groups)
 }
 
 # matematyka
-d1=read.table("student-mat.csv",sep=",",header=TRUE)
+d1=read.table("datasets/student-mat.csv",sep=",",header=TRUE)
 # portugalski
-d2=read.table("student-por.csv",sep=",",header=TRUE)
+d2=read.table("datasets/student-por.csv",sep=",",header=TRUE)
 # lista unikalnych studentów
 d3=merge(d1,d2,by=c("school","sex","age","address","famsize","Pstatus","Medu","Fedu","Mjob","Fjob","reason","nursery","internet"), suffix = c("_Mat", "_Por"))
 d3$Dalc = pmax(d3$Dalc_Mat,d3$Dalc_Por)
@@ -37,11 +39,11 @@ d3$Walc = pmax(d3$Walc_Mat,d3$Walc_Por)
 d3$Walc_Mat = NULL
 d3$Walc_Por = NULL
 
-png(file = "dalc_hist.png", width = 8, height = 8, units = 'in', res = 400)
+png(file = "histograms/dalc_hist.png", width = 8, height = 8, units = 'in', res = 400)
 hist(d3$Dalc, breaks=c(1,2,3,4,5), col="blue")
 dev.off()
 
-png(file = "walc_hist.png", width = 8, height = 8, units = 'in', res = 400)
+png(file = "histograms/walc_hist.png", width = 8, height = 8, units = 'in', res = 400)
 hist(d3$Walc, breaks=c(1,2,3,4,5), col="blue")
 dev.off()
 
@@ -53,7 +55,8 @@ selected_fatures <- na.omit(selected_fatures) # listwise deletion of missing
 for (dist_methode in c("euclidean", "maximum", "manhattan", "canberra", "binary", "minkowski")){
   for(clust_method in c("ward.D", "ward.D2", "single", "complete", "average", "mcquitty", "median", "centroid")){
     grouped <- data.frame(selected_fatures, group_id=dist_group(selected_fatures, 2, dist_methode, clust_method))#group_id=group(selected_fatures, 2))
-    png(file = paste("img/",clust_method,"_",dist_methode,"_grouping.png"), width = 8, height = 8, units = 'in', res = 400)
+    write.csv(grouped, file = paste("csv/",clust_method,"_",dist_methode,".csv", sep=""))
+    png(file = paste("grouping_plots/grouping/",clust_method,"_",dist_methode,".png", sep=""), width = 8, height = 8, units = 'in', res = 400)
     plot(grouped$Dalc, grouped$Walc, col=grouped$group_id)
     dev.off()
   }
@@ -64,10 +67,15 @@ for (dist_methode in c("euclidean", "maximum", "manhattan", "canberra", "binary"
 #############
 ### Bayes ###
 #############
+grouped = read.table("csv/ward.D_manhattan.csv",sep=",",header=TRUE)
 students = d3
 students$Dalc = NULL
 students$Walc = NULL
-students$Labels = as.factor(grouped$group_id)
+students$Label = as.factor(grouped$group_id)
+
+png(file = "histograms/grouping_hist.png", width = 8, height = 8, units = 'in', res = 400)
+hist(grouped$group_id, col="blue")
+dev.off()
 
 print("Badanie wpływu zmiany wartości K dla walidacji skrośnej na dokładność klasyfikacji")
 results = data.frame(matrix(ncol = 2, nrow = 0))
@@ -76,31 +84,34 @@ for (k in c(2, 5, 10, 20, 40, 60, 80, 100)){
     print(paste0("K = ", k))
     train_control = trainControl(method="repeatedcv", number=k, repeats = 50)
     grid = data.frame(fL=0, usekernel = TRUE, adjust=1)
-    model = train(Labels~., data=students, trControl=train_control, tuneGrid=grid, method="nb")
+    model = train(Label~., data=students, trControl=train_control, tuneGrid=grid, method="nb")
     newRow = data.frame(k_param=k , accuracy=model["results"][[1]]$Accuracy)
     results = rbind(results,newRow)
 }
-png(file = "k_accuracy.png", width = 8, height = 8, units = 'in', res = 500)
+png(file = "bayes_plots/k_accuracy.png", width = 8, height = 8, units = 'in', res = 500)
 plot(results$k_param, results$accuracy, type="o", xlab="Wartosc K", ylab="Dokladnosc")
 dev.off()
 
 print("Badanie wpływu ograniczania zestawu cech na dokładność klasyfikacji")
 results = data.frame(matrix(ncol = 2, nrow = 0))
-colnames(results) = c("feature", "accuracy")
-n = ncol(students) - 1;
-for(itt in 1:n) {
-    print(paste0("Rejecting: ", colnames(students)[itt]))
-    train_control = trainControl(method="repeatedcv", number=10, repeats=10)
+for(itt in 1:31) {
+    print(paste0("Rejecting: ", gsub("_Mat", "", colnames(students)[itt])))
+    train_control = trainControl(method="repeatedcv", number=20, repeats=10)
     reducedStudents = students
-    reducedStudents[itt] = NULL
+    if (itt < 14) {
+        reducedStudents[itt] = NULL
+    } else {
+        reducedStudents[itt + 18] = NULL
+        reducedStudents[itt] = NULL
+    }
     grid = data.frame(fL=0, usekernel = TRUE, adjust=1)
-    model = train(Labels~., data=reducedStudents, trControl=train_control, tuneGrid=grid, method="nb")
-    newRow = data.frame(feature=colnames(students)[itt] , accuracy=model["results"][[1]]$Accuracy)
+    model = train(Label~., data=reducedStudents, trControl=train_control, tuneGrid=grid, method="nb")
+    newRow = data.frame(feature=gsub("_Mat", "", colnames(students)[itt]), accuracy=model["results"][[1]]$Accuracy)
     results = rbind(results,newRow)
 }
-png(file = "reject_accuracy.png", width = 8, height = 8, units = 'in', res = 600)
+png(file = "bayes_plots/reject_accuracy.png", width = 8, height = 8, units = 'in', res = 600)
 plot(results$feature, results$accuracy, type="o", xlab="", xaxt="n", ylab="Dokladnosc")
-axis(1, at=1:n, labels=results$feature, las = 2, cex.axis = 0.66)
+axis(1, at=1:31, labels=results$feature, las = 2, cex.axis = 0.75)
 dev.off()
 
 
